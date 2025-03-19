2d0fc5d6c1ebe23200fd837fa3042d0dcc3459a9
Ternaskido <felixenesko@gmail.com>
2025-03-19 17:38:28 +0100

    Update bonus.py
    Program that add some percentage to salaries

--- bonus.py
+++ bonus.py
@@ -1,4 +1,4 @@
-# Using map, write a program that add 0.01 bonus to this salary
+# Program that add 0.01 bonus to salary
 salary = [2000, 4000, 6000, 8000]
 bonus = list(map(lambda x:x + 0.01, salary))
 print (bonus)
